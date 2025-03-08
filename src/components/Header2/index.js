78d043f04117e16b9b0784011d6c9b46f8d50098
Bharat Ambati <bharat.ambati@illusto.com>
2025-03-08 20:30:31 +0530

    New changes updated on header

--- src/components/Header2/index.js
+++ src/components/Header2/index.js
@@ -1,17 +1,35 @@
-import { Col, Row, Dropdown, Avatar, Divider } from 'antd';
-import { LogoutOutlined  } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Col, Row, Dropdown, Avatar, Divider, AutoComplete } from 'antd';
+import { LogoutOutlined, EnvironmentOutlined, UserAddOutlined, LoginOutlined  } from '@ant-design/icons';
 
 import styles from './style.module.css';
 
 const items = [
   {
-    key: '1',
+
+    label: 'Log in',
+    icon: <LoginOutlined style={{ fontSize: '16px' }} />,
+  },
+  {
+
+    label: 'Sign up',
+    icon: <UserAddOutlined style={{ fontSize: '16px' }} />
+  },
+  {
+
     label: 'Logout',
-    icon: <LogoutOutlined tyle={{ fontSize: '32px' }} />,
+    icon: <LogoutOutlined style={{ fontSize: '16px' }} />,
   },
 ];
 
+const mockVal = (str, repeat = 1) => ({
+  value: str.repeat(repeat),
+});
+
 function Header2() {
+  const [options, setOptions] = useState([]);
+  const getPanelValue = (searchText) => !searchText ? [] : [mockVal(searchText)];
+
   return (
     <header className={styles.header}>
       <Row className='height-100' justify='center' align="middle">
@@ -21,6 +39,17 @@ function Header2() {
               <a href="/" title="quicc.ai">
                 QC
               </a>
+              <Divider type='vertical' className={styles.logo_divider} />
+              <div className={styles.autocomplete_container}>
+                <EnvironmentOutlined style={{ fontSize: '16px' }} />
+                <AutoComplete
+                  options={options}
+                  placeholder="Location"
+                  onSearch={(text) => setOptions(getPanelValue(text))}
+                  variant="borderless"
+                  className={styles.autocomplete}
+                />
+              </div>
             </Col>
             <Col className={styles.profile_container}>
               <Dropdown
@@ -31,13 +60,15 @@ function Header2() {
                 trigger={['click']}
                 placement="bottomRight"
                 arrow
+                rootClassName={styles.profile_dropdown}
               >
                 <div className={styles.profile}>
-                  <small>
+                  {/* <small>
                     Welcome! <br />
                     Bharat Ambati
-                  </small>
-                  <Avatar shape='square' size={45} src="/assets/images/user.jpg" />
+                  </small> */}
+                  {/* <Avatar shape='square' size={45} src="/assets/images/user.jpg" /> */}
+                  <Avatar shape='square' size={45} src="/assets/images/default_user.jpg" />
                 </div>
               </Dropdown>
             </Col>
